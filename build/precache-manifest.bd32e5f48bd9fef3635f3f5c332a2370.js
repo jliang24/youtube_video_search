db90f1523d4fc3d6c13107db91f723ad77730a84
jliang24 <jeffliang@nevada.unr.edu>
2019-01-13 17:15:30 -0800

    deploy heroku

---
+++ build/precache-manifest.bd32e5f48bd9fef3635f3f5c332a2370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be473249350427b36960",
+    "url": "/static/js/main.be473249.chunk.js"
+  },
+  {
+    "revision": "26387354318e7d868eeb",
+    "url": "/static/js/1.26387354.chunk.js"
+  },
+  {
+    "revision": "be473249350427b36960",
+    "url": "/static/css/main.ea6fdf22.chunk.css"
+  },
+  {
+    "revision": "d9d46a31d2bbe63685358594767c9514",
+    "url": "/index.html"
+  }
+];
